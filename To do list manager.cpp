447834b5edb8e9509e8395af83e44d839b180b63
Bartek <bartek.jawor@onet.eu>
2025-04-05 01:17:38 +0200

    To do list manager.cpp

---
+++ To do list manager.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    cout << "Save/load (type 'Save' to save, 'Load' to load): " << endl;
+    string command;
+    getline(cin, command);
+
+    if (command == "Save" || command == "save") 
+    {
+        string task;
+        cout << "Give things to do: " << endl;
+        getline(cin, task);
+
+        fstream file;
+        file.open("ToDoList.txt", ios::in | ios::app);
+        
+        if (file.is_open()) 
+        {
+            file << task << endl;
+            cout << "Task saved!" << endl;
+            file.close();
+        } 
+        else 
+        {
+            cout << "Error opening file for saving!" << endl;
+        }
+
+    } 
+    else if (command == "Load" || command == "load") 
+    {
+        fstream tasks("ToDoList.txt");
+
+        if (tasks.is_open()) 
+        {
+            string text;
+            while (getline(tasks, text)) 
+            {
+                cout << text << endl;
+            }
+            tasks.close();
+        } 
+        else 
+        {
+            cout << "Error opening file!" << endl;
+        }
+    } 
+    else 
+    {
+        cout << "Invalid command!" << endl;
+    }
+
+    return 0;
+}
